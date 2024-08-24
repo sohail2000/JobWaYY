057287db4c4c60d68964720bd6ff10675723db3a
sohail2000 <msohail1379@gmail.com>
2024-08-24 23:55:48 +0530

    JobWaYY

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,28 @@
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💗 by{" "}
+        <a
+          href="https://github.com/sohail2000"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          Md Sohail
+        </a>
+      </div>
+
+    </div>
+  );
+};
+
+export default AppLayout;
